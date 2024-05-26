5bf70b3c95a21ef193f7d71dd3c080553d0be7b1
Haripriya <haripriya01212@gmail.com>
2024-05-26 08:47:04 +0530

    coding

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todo/screens/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    // making statusBar bg transparent
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo',
+      home: Home(),
+    );
+  }
+}
